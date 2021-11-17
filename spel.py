c82dfc14edb9c59b62bd848a2b8f0b62eea7df4b
WILLE HAGBERG <wille.hagberg@skola.taby.se>
2021-11-17 10:50:25 +0100

    Fixat med stats, healing och iv

--- spel.py
+++ spel.py
@@ -1,1 +1,30 @@
-print("spel")
+def main():
+
+    alive = True
+    svar = ""
+    hp = 100
+    level = 1
+    potion = 1
+
+    while alive:
+        svar = input("Vad vill du göra [d], [i], [s], [h] -->")
+        if hp == 0:
+            alive = False
+
+        if svar == "i":
+            print(f" Du har {potion} Healing Pottor")
+
+        if svar == "s":
+            print(f"HP:{hp}")
+            print(f"Level:{level}\n")
+
+        if svar == "h":
+            if potion == 0:
+                print("Du har inga pottor kvar")
+            else:
+                print("Du har tar en Healing Potta\n")
+                potion = potion - 1
+                hp = hp + 10
+
+
+main()
